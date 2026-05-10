36e727f4ef6825b88317dab28b29489e96394e1d
data-dmt <diegomntorres@gmail.com>
2026-05-10 17:51:22 +0100

    feat(business-unit): crear modulo base y alinear gson

---
+++ business-unit/src/main/java/org/ulpgc/paradiso/businessunit/Main.java
@@ -0,0 +1,7 @@
+package org.ulpgc.paradiso.businessunit;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("[BusinessUnit] Módulo inicializado. Sprint 3 en progreso.");
+    }
+}
